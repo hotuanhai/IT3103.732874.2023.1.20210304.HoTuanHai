3b86a838bbd85c6e8c6fcd652dca6825691f50e6
Hotuanhai <hotuanhai2412@gmail.com>
2023-11-15 12:55:13 +0700

    class mem

--- src/laboop/Lab02/DigitalVideoDisc.java
+++ src/laboop/Lab02/DigitalVideoDisc.java
@@ -1,55 +1,65 @@
 package laboop.Lab02;
 public class DigitalVideoDisc {
-    private String title;
-    private String category;
-    private String director;
-    private int length;
-    private float cost;
+    static String title;
+    static String category;
+    static String director;
+    static int length;
+    static float cost;
+    private static int nbDigitalVideoDiscs = 0;
 
     public String getTitle() {
         return title;    }
     public void setTitle(String title) {
-        this.title = title;    }
+        DigitalVideoDisc.title = title;    }
     public String getCategory() {
         return category;    }
     public void setCategory(String category) {
-        this.category = category;    }
+        DigitalVideoDisc.category = category;    }
     public String getDirector() {
         return director;    }
     public void setDirector(String director) {
-        this.director = director;    }
+        DigitalVideoDisc.director = director;    }
     public int getLength() {
         return length;    }
     public void setLength(int length) {
-        this.length = length;    }
+        DigitalVideoDisc.length = length;    }
     public float getCost() {
         return cost;    }
     public void setCost(float cost) {
-        this.cost = cost;    }
+        DigitalVideoDisc.cost = cost;    }
 
     public DigitalVideoDisc(String title) {
-        this.title = title;
+        DigitalVideoDisc.title = title;
+        DigitalVideoDisc.nbDigitalVideoDiscs++;
     }
     
     public DigitalVideoDisc(String title, String category, float cost) {
-        this.title = title;
-        this.category = category;
-        this.cost = cost;
+        DigitalVideoDisc.title = title;
+        DigitalVideoDisc.category = category;
+        DigitalVideoDisc.cost = cost;
+        DigitalVideoDisc.nbDigitalVideoDiscs++;
     }
 
     public DigitalVideoDisc(String title, String category, String director, float cost) {
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.cost = cost;
+        DigitalVideoDisc.title = title;
+        DigitalVideoDisc.category = category;
+        DigitalVideoDisc.director = director;
+        DigitalVideoDisc.cost = cost;
+        DigitalVideoDisc.nbDigitalVideoDiscs++;
     }
-
+    
+    public void info(){
+        System.out.println(title+" "+category+" "+director+" "+cost+" "+nbDigitalVideoDiscs);
+    }
+    
+    
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.length = length;
-        this.cost = cost;
+        DigitalVideoDisc.title = title;
+        DigitalVideoDisc.category = category;
+        DigitalVideoDisc.director = director;
+        DigitalVideoDisc.length = length;
+        DigitalVideoDisc.cost = cost;
+        DigitalVideoDisc.nbDigitalVideoDiscs++;
     }
 
     public DigitalVideoDisc() {
@@ -58,16 +68,5 @@ public class DigitalVideoDisc {
     @Override
     public boolean equals(Object obj) {
         return super.equals(obj); 
-    }
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    }    
 }
