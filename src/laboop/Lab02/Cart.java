e2a77a1ac59d734f193a3f4ec179572bca2c5f42
Hotuanhai <hotuanhai2412@gmail.com>
2023-11-15 10:39:31 +0700

    overload

--- src/laboop/Lab02/Cart.java
+++ src/laboop/Lab02/Cart.java
@@ -1,4 +1,7 @@
 package laboop.Lab02;
+
+import java.util.Arrays;
+
 public class Cart {
     private double cost=0;//tong tien
     public static final int MAX_NUMBERS_ORDERED = 20;
@@ -13,6 +16,53 @@ public class Cart {
             for(int i=0; i<MAX_NUMBERS_ORDERED;++i){    //them vao cart
                 if(itemsOrdered[i] == null){
                     itemsOrdered[i] = disc;
+                    cost += itemsOrdered[i].getCost();  //tang cost
+                    ++ qtyOrdered;                      //tang quantity
+                    System.out.println("success");
+                    break;
+                }   
+            }
+        }
+    }
+    
+    public void addDigitalVideoDisc(DigitalVideoDisc [] dvdList){
+        long a = Arrays.stream(dvdList).filter(e -> e != null).count();
+        if(qtyOrdered + a> MAX_NUMBERS_ORDERED ){          // check neu cart day
+            System.out.println("the cart is full");
+        }
+        else{
+            int index=0;
+            for(int i=0; i<MAX_NUMBERS_ORDERED;++i){    //them vao cart
+                if(itemsOrdered[i] == null){
+                    itemsOrdered[i] = dvdList[index];
+                    ++index;
+                    cost += itemsOrdered[i].getCost();  //tang cost
+                    ++ qtyOrdered;                      //tang quantity
+                    System.out.println("success");
+                    if(index == a) break;
+                }   
+            }
+        }
+        
+    }
+    
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1,DigitalVideoDisc dvd2){
+        if(qtyOrdered > MAX_NUMBERS_ORDERED - 1){          // check neu cart day
+            System.out.println("the cart is full");
+        }
+        else{
+            for(int i=0; i<MAX_NUMBERS_ORDERED;++i){    //them vao cart
+                if(itemsOrdered[i] == null){
+                    itemsOrdered[i] = dvd1;
+                    cost += itemsOrdered[i].getCost();  //tang cost
+                    ++ qtyOrdered;                      //tang quantity
+                    System.out.println("success");
+                    break;
+                }   
+            }
+            for(int i=0; i<MAX_NUMBERS_ORDERED;++i){    //them vao cart
+                if(itemsOrdered[i] == null){
+                    itemsOrdered[i] = dvd2;
                     cost += itemsOrdered[i].getCost();  //tang cost
                     ++ qtyOrdered;                      //tang quantity
                     System.out.println("success");
